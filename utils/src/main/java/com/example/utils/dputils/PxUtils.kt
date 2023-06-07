3ac9dfb2dd12399f9f27841914ec2805e7d6d82d
yuguochuqing <1257661537@qq.com>
2023-06-08 00:14:33 +0800

    Initial commit

---
+++ utils/src/main/java/com/example/utils/dputils/PxUtils.kt
@@ -0,0 +1,13 @@
+package com.example.utils.dputils
+
+import android.content.Context
+
+object PxUtils {
+    fun dpToPx(context: Context, dp: Int): Int {
+        return (context.resources.displayMetrics.density * dp + 0.5f).toInt()
+    }
+
+    fun spToPx(context: Context, sp: Int): Int {
+        return (context.resources.displayMetrics.scaledDensity * sp + 0.5f).toInt()
+    }
+}
